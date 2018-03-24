823775ceef059513172aade301d0d892d2a1c9fb
zecereal <zcrltv@gmail.com>
2018-03-25 00:46:46 +0700

    [EDIT] implement update() in playerController. Now, it can movable by touch event

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,14 +4,19 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
     public GameObject player;
+    public JoypadController joypad;
+    
 
-	// Use this for initialization
-	void Start () {
+    // Use this for initialization
+    void Start () {
 		
 	}
 	
 	// Update is called once per frame
 	void Update () {
-
-	}
+        //Input.GetAxis("Horizontal") *
+        var x = Time.deltaTime * 100.0f * joypad.GetTouchPosition.x;
+        var y = Time.deltaTime * 100.0f * joypad.GetTouchPosition.y;
+        transform.Translate(x, y, 0);
+    }
 }
