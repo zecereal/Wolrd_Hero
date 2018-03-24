544b570c2138e08863e2d59e793d9a1f36061c13
zecereal <zcrltv@gmail.com>
2018-03-24 22:37:36 +0700

    [EDIT] implement joypadController from simpleTouchController

--- Assets/scripts/joypadController.cs
+++ Assets/scripts/joypadController.cs
@@ -1,16 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
 
-public class joypadController : MonoBehaviour {
+public class joypadController : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    // PUBLIC
+    public delegate void TouchDelegate(Vector2 value);
+    public event TouchDelegate TouchEvent;
+
+    public delegate void TouchStateDelegate(bool touchPresent);
+    public event TouchStateDelegate TouchStateEvent;
+
+    // PRIVATE
+    [SerializeField]
+    private RectTransform joystickArea;
+    private bool touchPresent = false;
+    private Vector2 movementVector;
+
+
+    public Vector2 GetTouchPosition
+    {
+        get { return movementVector; }
+    }
+
+
+    public void BeginDrag()
+    {
+        touchPresent = true;
+        if (TouchStateEvent != null)
+            TouchStateEvent(touchPresent);
+    }
+
+    public void EndDrag()
+    {
+        touchPresent = false;
+        movementVector = joystickArea.anchoredPosition = Vector2.zero;
+
+        if (TouchStateEvent != null)
+            TouchStateEvent(touchPresent);
+
+    }
+
+    public void OnValueChanged(Vector2 value)
+    {
+        if (touchPresent)
+        {
+            // convert the value between 1 0 to -1 +1
+            movementVector.x = ((1 - value.x) - 0.5f) * 2f;
+            movementVector.y = ((1 - value.y) - 0.5f) * 2f;
+
+            if (TouchEvent != null)
+            {
+                TouchEvent(movementVector);
+            }
+        }
+
+    }
+
 }
