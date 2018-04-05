fa17df21aabab5a0c10ad5ba10eedb3d12ee527c
zecereal <zcrltv@gmail.com>
2018-04-05 15:23:04 +0700

    [ADD] character can move all directions

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -6,11 +6,13 @@ public class PlayerController : MonoBehaviour {
     public GameObject player;
     public JoypadController joypad;
     public Animator anim;
+    public bool isRight;
     
 
     // Use this for initialization
     void Start () {
         anim = gameObject.GetComponent<Animator>();
+        isRight = true;
 	}
 	
 	// Update is called once per frame
@@ -19,14 +21,27 @@ public class PlayerController : MonoBehaviour {
         var x = Time.deltaTime * 100.0f * joypad.GetTouchPosition.x;
         var y = Time.deltaTime * 100.0f * joypad.GetTouchPosition.y;
         transform.Translate(x, y, 0);
-
-        if (joypad.GetTouchPosition.x!=0)
+        if (joypad.GetTouchPosition.x > 0)
         {
+            isRight = true;
             anim.Play("walk_R");
         }
-        else
+        else if (joypad.GetTouchPosition.x < 0)
         {
-            anim.Play("idle_R");
+            isRight = false;
+            anim.Play("walk_L");
+        }
+
+        if (joypad.GetTouchPosition.x ==0 && joypad.GetTouchPosition.y==0)
+        {
+            if(isRight)
+            {
+                anim.Play("idle_R");
+            }
+            else
+            {
+                anim.Play("idle_L");
+            } 
         }
     }
 }
