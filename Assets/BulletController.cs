57d75b884adc92ad219892535ec0908a416d25aa
zecereal <zcrltv@gmail.com>
2018-06-23 23:04:04 +0700

    [EDIT] DASH cooldown and [ADD] init bullet when fire

---
+++ Assets/BulletController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletController : MonoBehaviour {
+    public float velX;
+    public float velY;
+
+    private Rigidbody2D rb;
+    // Use this for initialization
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Destroy(gameObject, 0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rb.velocity = new Vector2(velX, velY);
+
+    }
+}
