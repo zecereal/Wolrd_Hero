17a3f147138e93227b79c1367f8291055d9abe10
Jackrit Yangsung <zcrltv@gmail.com>
2018-10-17 23:02:44 +0700

    [ADD] Spawner Crete/enemy/boss

--- Assets/scripts/spawnerController.cs
+++ Assets/scripts/spawnerController.cs
@@ -1,19 +1,29 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class spawnerController : MonoBehaviour {
 
 	public GameObject crete;
 	public GameObject enemy;
+
 	public GameObject boss;
 
+	public GameObject boss_hp;	
 	public int enemyQuantity;
-	public bool isBossZone;
 
+	public bool isEnemySpawner;
+	public bool isCreteSpawner;
+	public bool isBossZone;
 	private int enemyStack;
 	private Vector2 position;
 
+	void Awake()
+	{
+		boss_hp.SetActive(false);
+		
+	}
+	
 	void spawnEnemy(int quantity){
 		enemyStack = quantity;
 		position = transform.position;
@@ -21,17 +31,24 @@ public class spawnerController : MonoBehaviour {
 	}
 
 	void initNextEnemy(){
+		int randomSpace = Random.Range(3,5);
 		if(enemyStack > 0){
-			GameObject temp = Instantiate (enemy, new Vector2(position.x + 10 + enemyStack ,position.y) , Quaternion.identity);
+			Debug.Log(randomSpace);
+			GameObject temp = Instantiate (enemy, new Vector2(position.x + 10 + (enemyStack*randomSpace) ,position.y) , Quaternion.identity);
 			temp.name = ""+this.name+enemyStack;
 			enemyStack--;
 			initNextEnemy();
 		}
 	}
-	void spawnBoss(){
 
+	void spawnCrete(){
+		Instantiate (crete, new Vector2(position.x + 25 ,position.y) , Quaternion.identity);
 	}
 
+	void spawnBoss(){
+		Instantiate (boss, new Vector2(position.x + 25 ,position.y) , Quaternion.identity);
+		boss_hp.SetActive(true);
+	}
 	private void OnCollisionEnter2D (Collision2D other) {
 
 		if (other.collider.CompareTag ("Player")) {
@@ -39,6 +56,7 @@ public class spawnerController : MonoBehaviour {
 		}
 
 		if(enemyQuantity!=0) spawnEnemy(enemyQuantity);
+		if(isCreteSpawner) spawnCrete();
 		if(isBossZone) spawnBoss();
 		
 	}
