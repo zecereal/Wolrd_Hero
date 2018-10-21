4babbfb5a2ae3af438426adaf418880146f5feae
Jackrit Yangsung <zcrltv@gmail.com>
2018-10-21 20:00:21 +0700

    [ADD] player can press "pause" button to pause game and resume by pressing "resume" button

---
+++ Assets/scripts/interface/PauseController.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseController : MonoBehaviour {
+    
+    [SerializeField] private GameObject pause_panel;
+    [SerializeField] private bool isPause;
+    
+    public void activatePausePanel(){
+        Time.timeScale = 0;
+        pause_panel.SetActive(true);
+        isPause = true;
+    }
+
+    public void deactivatePausePanel(){
+        Time.timeScale = 1;
+        pause_panel.SetActive(false);
+        isPause = false;
+    }
+}
