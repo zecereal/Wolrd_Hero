ddc01bf1deb9e9c37041f7f0b7723851f134819b
Jackrit Yangsung <zcrltv@gmail.com>
2018-10-24 14:26:46 +0700

    [ADD] player can surrender and quit the game

--- Assets/scripts/interface/PauseController.cs
+++ Assets/scripts/interface/PauseController.cs
@@ -3,20 +3,41 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PauseController : MonoBehaviour {
-    
+
     [SerializeField] private GameObject pause_panel;
+    [SerializeField] private GameObject serrender_panel;
     [SerializeField] private bool isPause;
-    
-    public void activatePausePanel(){
-        Time.timeScale = 0;
-        pause_panel.SetActive(true);
+
+    public void activatePausePanel () {
+        pause_panel.SetActive (true);
         isPause = true;
     }
 
-    public void deactivatePausePanel(){
-        Time.timeScale = 1;
-        pause_panel.SetActive(false);
+    public void deactivatePausePanel () {
+        pause_panel.SetActive (false);
         isPause = false;
+    }
+
+    public void activateSurrenderPanel () {
+        pause_panel.SetActive (false);
+        serrender_panel.SetActive (true);
+    }
+
+    public void deactivateSurrenderPanel () {
+        serrender_panel.SetActive (false);
+        isPause = false;
+    }
+
+    public void quitGame () {
+        Application.Quit();
+    }
+
+    void Update () {
+        if (isPause) {
+            Time.timeScale = 0;
+        } else {
+            Time.timeScale = 1;
+        }
     }
 
 }
